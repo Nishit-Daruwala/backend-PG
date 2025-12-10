68a58ec55f93fe23fd92f89126976f21b929a2f2
Nishit Daruwala <nishitdaruwala@gmail.com>
2025-12-10 21:40:51 +0530

    fix server

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use("/api/bookings", bookingRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
